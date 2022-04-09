8066ca81d622ee39e177b6cd17a48e7cfc04aab0
Even Stensberg <evenstensberg@gmail.com>
2022-04-09 19:37:56 +0200

    chore: init

---
+++ README.md
@@ -0,0 +1,16 @@
+## lua-get-random-word
+
+Get random word from a list of words. Comma seperated.
+
+## Usage
+
+### With symlink (sudo chmod 750 main.lua)
+```console
+$ ./main.lua
+```
+
+### Without symlink
+
+```console
+$ lua ./main.lua
+```
---
+++ main.lua
@@ -0,0 +1,22 @@
+#!/usr/bin/env lua
+
+print("Please type in words seperated by , to get random word: \n")
+
+local string_unformatted = io.read()
+local n_elements = 1
+local words = {}
+
+for key in string.gmatch(string_unformatted, '([^,]+)') do
+    words[n_elements] = key
+    n_elements = n_elements + 1
+end
+
+
+--[[ Seed with current date ]]
+math.randomseed(os.time())
+
+--[[ Random word idx ]]
+local rand_idx = math.random(1, n_elements - 1)
+
+
+print("Selected word: ", words[rand_idx])
